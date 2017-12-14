5c97e4431d2be834ada87eee85f45dc2546b31d0
jialangji <1401319133@qq.com>
2017-12-14 20:38:50 +0800

    建包

---
+++ app/src/main/java/code/xp/mysocialappteam/present/MyPresent.java
@@ -0,0 +1,8 @@
+package code.xp.mysocialappteam.present;
+
+/**
+ * Created by dell on 2017/12/14.
+ */
+
+public class MyPresent {
+}
---
+++ app/src/main/java/code/xp/mysocialappteam/view/MyView.java
@@ -0,0 +1,8 @@
+package code.xp.mysocialappteam.view;
+
+/**
+ * Created by dell on 2017/12/14.
+ */
+
+public class MyView {
+}
