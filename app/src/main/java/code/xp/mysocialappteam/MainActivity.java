b65a3d2878fe8356ef1ad0ddd3028b125ff4b3dd
jialangji <1401319133@qq.com>
2017-12-15 10:48:21 +0800

    建包

--- app/src/main/java/code/xp/mysocialappteam/MainActivity.java
+++ app/src/main/java/code/xp/mysocialappteam/MainActivity.java
@@ -1,24 +1,69 @@
 package code.xp.mysocialappteam;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import code.xp.mysocialappteam.fragment.Yd2Fragment;
+import code.xp.mysocialappteam.fragment.YdFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mVp;
+    private List<Fragment> fragments;
+    private SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //这是我的团队
+        initView();
+        sp = getSharedPreferences("config",MODE_PRIVATE);
+        if (sp.getBoolean("flag",false)){
+             return;
+        }
+        sp.edit().putBoolean("flag",true).commit();
+        fragments=new ArrayList<>();
+        YdFragment ydFragment=new YdFragment();
+        Yd2Fragment yd2Fragment =new Yd2Fragment();
+        fragments.add(ydFragment); fragments.add(yd2Fragment);
+        mVp.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return fragments.get(position);
+            }
 
-        //阿打算打算
+            @Override
+            public int getCount() {
+                return fragments.size();
+            }
+        });
 
+//三秒跳转
+//       new Thread(){
+//           int num=3;
+//            @Override
+//            public void run() {
+//                for (int i = num; i >0 ; i--){
+//                    try {
+//                        Thread.sleep(1000);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//                Intent intent=new Intent(MainActivity.this,);
+//                startActivity(intent);
+//            }
+//        }.start();
+    }
 
-
-
-
-
-
-
+    private void initView() {
+        mVp = (ViewPager) findViewById(R.id.vp);
     }
 }
