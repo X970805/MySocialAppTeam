289978fef4b7dc18dced5a665cd9cc52661365fc
jialangji <1401319133@qq.com>
2017-12-15 10:56:38 +0800

    修改

---
+++ app/src/main/java/code/xp/mysocialappteam/fragment/Yd2Fragment.java
@@ -0,0 +1,48 @@
+package code.xp.mysocialappteam.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import code.xp.mysocialappteam.R;
+
+/**
+ * Created by dell on 2017/12/15.
+ */
+
+public class Yd2Fragment extends Fragment implements View.OnClickListener {
+
+    private Button mExperienceBtn;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.yd2_fragment, null);
+        initView(view);
+        return view;
+    }
+
+    private void initView(@NonNull final View itemView) {
+        mExperienceBtn = itemView.findViewById(R.id.btn_experience);
+        mExperienceBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_experience:
+                Toast.makeText(getActivity(), "进入", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/code/xp/mysocialappteam/fragment/YdFragment.java
@@ -0,0 +1,24 @@
+package code.xp.mysocialappteam.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import code.xp.mysocialappteam.R;
+
+/**
+ * Created by dell on 2017/12/15.
+ */
+
+public class YdFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.yd1_fragment,null);
+        return view;
+    }
+}
